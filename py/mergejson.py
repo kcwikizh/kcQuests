adea9451c32ccc676e319fc774caa0b5a49c8ecb
callofblood <70617760+callofblood@users.noreply.github.com>
2022-02-20 14:11:14 +0800

    Update mergejson.py

--- py/mergejson.py
+++ py/mergejson.py
@@ -26,3 +26,30 @@ output.write(str(rs))
 
 output=open('./dist/quests-scn.json','w+',encoding='utf-8')
 output.write(str(rs))
+
+
+# 单独生成最新
+
+
+new=2
+num=0
+newrs={}
+while(num!=100):
+	if(os.path.isfile('../fineJson/'+str(new)+'-'+str(num)+'.json')):
+		print("new"+str(num))
+		temp=open('../fineJson/'+str(new)+'-'+str(num)+'.json','r+',encoding='utf-8')
+		content=temp.read()
+		content=json.loads(content)
+		for i in content:
+			newrs[i]=content[i]
+	num+=1
+
+newrs=str(newrs).replace("'",'"')
+newrs=json.loads(newrs)
+newrs=json.dumps(newrs,sort_keys=True,ensure_ascii=False,indent=2)	
+
+
+output=open('./quests-scn-new.json','w+',encoding='utf-8')
+output.write(str(newrs))
+output=open('./dist/quests-scn-new.json','w+',encoding='utf-8')
+output.write(str(newrs))
