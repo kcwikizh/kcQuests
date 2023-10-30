1749745dd05434a5d10612db2dfba00ce216ce30
Andy Chu <andy.chu.cs@icloud.com>
2023-10-30 21:26:48 +0800

    fix: only ascii characters in pre (#13)
    * fix: only ascii characters in pre
    
    * GitHub Actions Refresh
    
    ---------
    
    Co-authored-by: GitHub Actions Bot <>

--- src/kcwiki/quest.py
+++ src/kcwiki/quest.py
@@ -49,7 +49,7 @@ class Quest:
             value = arg.value.strip()
             if name == '编号':
                 quest.code = value
-            if '前置' in name and value:
+            if '前置' in name and value.encode().isalnum():
                 quest.pre.append(value)
             if name == '中文任务名字':
                 quest.name = filter_text(value, WT_FILTERS)
